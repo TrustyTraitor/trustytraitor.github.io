422a919c2d94519ce2ffa626ba29c171072c98c2
Dovahkid <gainmikey@gmail.com>
2018-12-28 01:13:16 -0500

    start

--- scripts.js
+++ scripts.js
@@ -1,19 +1,1 @@
-function clickSpider(){
 
-  if(document.getElementById("spideraccuse").getAttribute("src") === 'spidermanaccuse.jpeg')
-  {
-    document.getElementById("spideraccuse").src = "spidergotya.jpg";
-  } else {
-    document.getElementById("spideraccuse").src = "spidermanaccuse.jpeg";
-  }
-
-}
-
-function spiderHover(){
-
-  if(document.getElementById("spideraccuse").getAttribute("src") === "spidermanaccuse.jpeg"){
-    alert("Spiderman cannot trust anyone, not even himself");
-  } else {
-    alert("Spiderman says you're ok!");
-  }
-}
