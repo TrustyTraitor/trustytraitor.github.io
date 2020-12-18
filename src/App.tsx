10711a5a6dc63c6a1925d373dabd3deb2bcbe01e
MichaelG <gainmikey@gmail.com>
2020-12-18 13:38:50 -0500

    hopefully fixed routing 2

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
 
           <Navigation/>
           
