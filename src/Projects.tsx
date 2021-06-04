321355980695feb5965ff2224bcbd93eb4e92eb5
Dovahkid <35874258+Dovahkid@users.noreply.github.com>
2021-06-04 14:38:01 -0400

    Added jumbotron to projects page
    Added jumbotron with description for the projects page

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import PanelCollection from './Components/PanelCollection';
 import getRepos from './scripts/getRepos';
+import {Jumbotron} from 'react-bootstrap';
 
 const Projects = () =>
 {
@@ -14,6 +15,12 @@ const Projects = () =>
     return (
         repos.length ? 
         <div className="container">
+            <Jumbotron>
+                <h1>My Projects</h1>
+                <p>This page contains links and short descriptions for each of my github projects. 
+                    The list is not curated but is instead automatically pulled when the page is loaded. 
+                    Web projects will have links to the site and all projects will have links to the repos.</p>
+            </Jumbotron>
             <PanelCollection repos={repos}/>
         </div>
         :
